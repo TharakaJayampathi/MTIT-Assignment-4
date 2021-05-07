c3b9bf6b90bed7ba9d1b0358b91bb70b2c44960d
TharakaJayampathi <tharakajayampathikaranahaluge@gmail.com>
2021-05-08 00:03:53 +0530

    init

---
+++ delivery-service/src/test/java/com/sliit/mtit62/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit62.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
