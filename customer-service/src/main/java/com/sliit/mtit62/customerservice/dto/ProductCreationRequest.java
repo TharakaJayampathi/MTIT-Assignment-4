9b185ff75486d6b6eb52908e4c942e6da542bcce
ravindujeewantha <ravinduj@apium.io>
2021-05-10 22:23:41 +0530

    Created microservises

---
+++ customer-service/src/main/java/com/sliit/mtit62/customerservice/dto/ProductCreationRequest.java
@@ -0,0 +1,2 @@
+package com.sliit.mtit62.customerservice.dto;public class ProductCreationRequest {
+}
---
+++ customer-service/src/main/java/com/sliit/mtit62/customerservice/service/CustomerServiceImpl.java
@@ -0,0 +1,2 @@
+package com.sliit.mtit62.customerservice.service;public class CustomerServiceImpl {
+}
