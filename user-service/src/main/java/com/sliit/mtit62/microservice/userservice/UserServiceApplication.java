912d34ba295989fd628f3fe455b4879c20dbb006
TharakaJayampathi <tharakajayampathikaranahaluge@gmail.com>
2021-05-10 19:27:52 +0530

    user-service created

---
+++ user-service/src/main/java/com/sliit/mtit62/microservice/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit62.microservice.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
